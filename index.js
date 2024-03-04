558eca7fc83764944a66f2956f2b122e7b36f52b
Your Name <you@example.com>
2024-03-04 16:34:05 +0800

    请求跨域失败问题，增加text字段假数据

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors'); // 引入cors包
 const app = express();
 
 const PORT = 3334;
+const OCR_RESULT = `\\begin{tabular}{|c|c|}\n\\hline$x$ & $y$ \\\\\n\\hline 20 & 12.4 \\\\\n30 & 24.8 \\\\\n40 & 49.6 \\\\\n50 & 99.2 \\\\\n60 & 198.4 \\\\\n70 & 396.8 \\\\\n80 & 793.6 \\\\\n\\hline\n\\end{tabular}`;
 
+app.use(cors()); // 在这里使用cors中间件，允许所有跨域请求
 app.use(express.json()); // 用于解析JSON格式的请求体
 
 // 提供一个名为 upload_image 的POST接口
@@ -14,9 +17,10 @@ app.post('/upload_image', (req, res) => {
         status: 'success',
         message: 'Image uploaded successfully',
         // 这里可以添加更多返回信息，如图片的存储信息等
+        text:OCR_RESULT,
     });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://39.105.195.249:${PORT}`);
+    console.log(`Server is running on xxx:${PORT}`);
 });
